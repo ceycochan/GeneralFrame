8004d7783d5674d013d0eb4beec1a1a12cf4ebfb
ceycochan <gumptionchan@163.com>
2017-12-16 18:08:13 +0800

    Initial commit

---
+++ app/src/main/java/com/nshane/generalframe/MainActivity.java
@@ -0,0 +1,20 @@
+package com.nshane.generalframe;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+    /**
+     *
+     * @author
+     *
+     * @description  a collecting frame for app constructing 
+     *
+     */
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+}
